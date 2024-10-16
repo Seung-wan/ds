858fc8a758b01c927f9084ebd1622b3857af9d4a
Seung-wan <tmddhks0104@gmail.com>
2024-10-17 01:21:35 +0900

    빌드 config 설정들을 수정합니다.

--- vite.config.ts
+++ vite.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
               syntax: 'typescript',
               tsx: true,
             },
-            target: 'esnext',
+            target: 'es2015',
           },
         }),
       ],
