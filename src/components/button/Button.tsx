fa28f7b24cb3bedc95f3dabe6e80c429aff5a6fd
Seung-wan <tmddhks0104@gmail.com>
2024-10-23 02:01:32 +0900

    Button 컴포넌트에 isLoading prop을 추가합니다.

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,17 +1,34 @@
 import { ComponentPropsWithoutRef, forwardRef, PropsWithChildren } from 'react';
+import classnames from 'classnames';
 import { Slot, Slottable } from '../common/slot';
+
+import '@/index.css';
+import Spinner from '../spinner/Spinner';
 
 interface ButtonProps extends ComponentPropsWithoutRef<'button'> {
   asChild?: boolean;
+  isLoading?: boolean;
 }
 
 const Button = forwardRef<HTMLButtonElement, PropsWithChildren<ButtonProps>>(
-  ({ asChild, children, ...restProps }, ref) => {
+  (
+    { asChild, className, isLoading = false, children, disabled, ...restProps },
+    ref
+  ) => {
     const Element = asChild ? Slot : 'button';
 
     return (
-      <Element ref={ref} {...restProps}>
-        <Slottable>{children}</Slottable>
+      <Element
+        className={classnames(
+          'border border-gray-300 p-2 w-[120px]',
+          { 'flex justify-center': isLoading },
+          className
+        )}
+        ref={ref}
+        disabled={disabled || isLoading}
+        {...restProps}
+      >
+        {isLoading ? <Spinner size="md" /> : <Slottable>{children}</Slottable>}
       </Element>
     );
   }
